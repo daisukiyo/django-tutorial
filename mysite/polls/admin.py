54f7dcbd57a624194f4612874d75a1a41aa2cdb6
daisukiyo <tk.rain09@gmail.com>
2019-05-31 16:56:45 -0700

    grant poll questions an admin interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Question
 # Register your models here.
+
+# tell admin that questin objects have an admin interface
+admin.site.register(Question)
